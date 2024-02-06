c64187127996ee064e54f77195bf91d21d32d0cb
suhail khan <khansuhail9598@gmail.com>
2024-02-06 20:49:12 +0530

    info card setup

--- raj_police/src/views/home/Home.jsx
+++ raj_police/src/views/home/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { images } from "../../utils/data";
 import Carousel from "./Carousel";
+import { FiEye } from "react-icons/fi";
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { LuUserCheck } from "react-icons/lu";
+import "./home.css";
+
 const Home = () => {
     console.log(images);
 
@@ -9,25 +13,37 @@ const Home = () => {
         <div className="carosal_div_container">
             <Carousel images={images} />
             <div className="card_div">
-                <div>
-                    <FaArrowUpRightFromSquare />
+                <div className="card">
+                    <FaArrowUpRightFromSquare className="arrow" />
+
                     <span>Crime Analytics</span>
-                    <button>Open</button>
+                    <button id="btn">
+                        Open <FiEye className="eye-icon" />
+                    </button>
                 </div>
-                <div>
-                    <FaArrowUpRightFromSquare />
+                <div className="card">
+                    <FaArrowUpRightFromSquare className="arrow" />
+
                     <span>Crime Analytics</span>
-                    <button style={{backgroundColor:"red"}}>Open</button>
+                    <button id="btn">
+                        Open <FiEye className="eye-icon" />
+                    </button>
                 </div>
-                <div>
-                    <FaArrowUpRightFromSquare />
-                    <span>Crime Analytics</span>
-                    <button>Open</button>
+                <div className="card">
+                    <LuUserCheck className="arrow" />
+
+                    <span>Citizen Services And Information</span>
+                    <button id="btn">
+                        Open <FiEye className="eye-icon" />
+                    </button>
                 </div>
-                <div>
-                    <FaArrowUpRightFromSquare />
+                <div className="card">
+                    <FaArrowUpRightFromSquare className="arrow" />
+
                     <span>Crime Analytics</span>
-                    <button>Open</button>
+                    <button id="btn">
+                        Open <FiEye className="eye-icon" />
+                    </button>
                 </div>
             </div>
         </div>
